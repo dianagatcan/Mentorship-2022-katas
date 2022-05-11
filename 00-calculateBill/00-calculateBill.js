8a8a73804f06213438d94eb1f7f222df06a3ebea
dianagatcan <gatcan.diana@gmail.com>
2022-05-11 12:04:38 +0300

    ✅ calculate Bill test

--- 00-calculateBill/00-calculateBill.js
+++ 00-calculateBill/00-calculateBill.js
@@ -1,4 +1,7 @@
 function CalculateBill(bill, vat, tip){
+    const total = bill + (bill * vat /100) + (bill * tip / 100)
+    //Format total as a currency string with Intl.NumberFormat
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total);
 }
 
 module.exports = CalculateBill;
